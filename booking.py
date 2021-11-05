1f7997e18ebe72b1c84936d6953f90bf5282a53e
Emma Rock <erock@flipboard.com>
2021-11-05 12:33:52 -0400

    initial commit

---
+++ booking.py
@@ -0,0 +1,101 @@
+
+
+class ShipmentStatus(object):
+    def __init__(self):
+        self.status_date = 
+        
+        self.shipment = None
+        
+    # Start of user code -> properties/constructors for ShipmentStatus class
+
+    # End of user code
+    # Start of user code -> methods for ShipmentStatus class
+
+    # End of user code
+
+class ShippingMode(object):
+    def __init__(self):
+        
+        self.shipment = None
+        
+    # Start of user code -> properties/constructors for ShippingMode class
+
+    # End of user code
+    # Start of user code -> methods for ShippingMode class
+
+    # End of user code
+
+class Shipment(object):
+    def __init__(self):
+        self.shipment_number = 
+        self.origin_port = 
+        self.destination_port = 
+        
+        self.shipmentStatus = None
+        self.shippingMode = None
+        
+    # Start of user code -> properties/constructors for Shipment class
+
+    # End of user code
+    # Start of user code -> methods for Shipment class
+
+    # End of user code
+
+class TruckShipment(ShippingMode):
+    pass
+    # Start of user code -> properties/constructors for TruckShipment class
+
+    # End of user code
+    # Start of user code -> methods for TruckShipment class
+
+    # End of user code
+
+class Shipping(ShipmentStatus):
+    pass
+    # Start of user code -> properties/constructors for Shipping class
+
+    # End of user code
+    # Start of user code -> methods for Shipping class
+
+    # End of user code
+
+class Quote(ShipmentStatus):
+    pass
+    # Start of user code -> properties/constructors for Quote class
+
+    # End of user code
+    # Start of user code -> methods for Quote class
+
+    # End of user code
+
+class Booking(ShipmentStatus):
+    pass
+    # Start of user code -> properties/constructors for Booking class
+
+    # End of user code
+    # Start of user code -> methods for Booking class
+
+    # End of user code
+
+class OceanShipment(ShippingMode):
+    pass
+    # Start of user code -> properties/constructors for OceanShipment class
+
+    # End of user code
+    # Start of user code -> methods for OceanShipment class
+
+    # End of user code
+
+class AirShipment(ShippingMode):
+    pass
+    # Start of user code -> properties/constructors for AirShipment class
+
+    # End of user code
+    # Start of user code -> methods for AirShipment class
+
+    # End of user code
+
+
+# Start of user code -> functions/methods for booking package
+
+# End of user code
